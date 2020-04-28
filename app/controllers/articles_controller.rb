57a385cc36a2ad2862a8421e12b28e74ae87d761
Logan MacLaren <maclarel@github.com>
2020-04-28 17:46:28 -0400

    April 28th updates - edit/delete articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,16 +11,35 @@ class ArticlesController < ActionController::Base
     @article = Article.new
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   def create
     @article = Article.new(params.require(:article).permit(:title, :description))
-    #render plain: @article.inspect
     if @article.save
       flash[:notice] = "Article was created successfully."
-      #byebug
       redirect_to @article
     else
       render 'new'
     end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title, :description))
+      flash[:notice] = "Article was edited successfully."
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    flash[:notice] = "Article was deleted successfully."
+    redirect_to articles_path
   end
 
 end
