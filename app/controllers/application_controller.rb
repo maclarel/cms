2c5348a03e73ad01b448a7155881b6258ab68cda
Logan MacLaren <maclarel@github.com>
2020-07-14 16:52:11 -0400

    including notes about Heroku-specific workaround

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,9 @@ class ApplicationController < ActionController::Base
   helper_method :current_user, :logged_in?
 
   def current_user
+    # Note that this will do a database lookup every time.
+    # This is not ideal, but seems to be the only way to get
+    # this to work on Heroku right now.
     User.find(session[:user_id]) if session[:user_id]
   end
 
